bbf2a3fbef23392fd3ea491664479190ac1eb792
Павел <@192.168.0.4>
2016-09-22 10:43:35 +0600

    final static

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,19 +1,31 @@
-var width = parseFloat($('body').css('width')),
-    sliders = $('.slider'),
-    slides = $('.slides'),
-    sliderControl = $('.slider__control-item'),
-    sliderControlFirst = $('.slider__control-item:first-child');
+    var width = parseFloat($('body').css('width')),
+        sliders = $('.slider'),
+        slides = $('.slides'),
+        sliderControl = $('.slider__control-item'),
+        sliderControlFirst = $('.slider__control-item:first-child'),
+        appeal = $('.appeal__main');
 
-$(function adaptive() {    
-    $(window).resize(function() {
-        width = parseFloat($('body').css('width'));
-        slides.css('right', 0);
-        sliderControl.removeClass('slider__control-item--active');
-        sliderControlFirst.addClass('slider__control-item--active');
+function appealTop() {
+    appeal.each(function(i, el) {
+        var header = parseFloat($(el).find('h2').css('height')),
+            rectangle = parseFloat($('.appeal__rectangle').css('height'));
+        if (width >= 1263) {
+            if (width > 1423 && width < 1900)
+                width = 1440;
+            else if (width >= 1903)
+                width = 1903;
+            if (header < rectangle) {
+                $(el).css('padding-top', width/5 - (header + (rectangle - header)/2));
+            } else if (header > rectangle) {
+                $(el).css('padding-top', width/5 - (header - (header - rectangle)/2));
+            }            
+        } else if (width < 1263) {
+            $(el).css('padding-top', '');
+        } 
     });
-});
+}
 
-$(function slider() {
+function slider() {
     if (width < 768) {
         sliders.each(function(i, slider) {
             var slide = $(slider).find('.slider__slide'),
@@ -50,4 +62,21 @@ $(function slider() {
             });
         });
     }
+}
+
+$(document).ready(function() {  
+   appealTop();
+    slider(); 
+});
+
+
+$(window).resize(function() {
+    width = parseFloat($('body').css('width'));
+    
+    slides.css('right', 0);
+    sliderControl.removeClass('slider__control-item--active');
+    sliderControlFirst.addClass('slider__control-item--active');
+    
+    appealTop();
+    slider();
 });
